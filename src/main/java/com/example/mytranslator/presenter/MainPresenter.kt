2eec4d6aef01cfca54f4b7cb0cbb5e9a2f4bd379
andrey <a.serbin@litebox.ru>
2022-03-29 21:59:08 +0400

    add new lib, replace model in presenter, implement getData()

--- src/main/java/com/example/mytranslator/presenter/MainPresenter.kt
+++ src/main/java/com/example/mytranslator/presenter/MainPresenter.kt
@@ -1,12 +1,15 @@
 package com.example.mytranslator.presenter
 
 import com.example.mytranslator.MainContract
-import com.example.mytranslator.model.LocalModel
+import com.example.mytranslator.model.RemoteModel
+import com.example.mytranslator.model.WordTranslate
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainPresenter<V : MainContract.View> : MainContract.Presenter<V> {
 
     private var currentView: V? = null
-    private val model = LocalModel()
+    private val model = RemoteModel()
 
     override fun attachView(view: V) {
         if (view != currentView) {
@@ -21,6 +24,20 @@ class MainPresenter<V : MainContract.View> : MainContract.Presenter<V> {
     }
 
     override fun getData(word: String) {
-        currentView?.showUsers(model.getData().blockingGet())
+        model.getData(word)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                currentView?.showUsers(
+                    it.map { data ->
+                        WordTranslate(
+                            data.text ?: "",
+                            data.meanings?.get(0)?.translation?.translation ?: ""
+                        )
+                    }
+                        .toList())
+            }, {
+                it.message?.let { err -> currentView?.showError(err) }
+            })
     }
 }
