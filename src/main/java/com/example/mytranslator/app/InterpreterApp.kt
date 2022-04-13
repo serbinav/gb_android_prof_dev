2d39ed701d531e91bc422eb50dc484cec5efd525
andrey <a.serbin@litebox.ru>
2022-04-13 23:22:10 +0400

    fix crash add androidContext
    Signed-off-by: andrey <a.serbin@litebox.ru>

--- src/main/java/com/example/mytranslator/app/InterpreterApp.kt
+++ src/main/java/com/example/mytranslator/app/InterpreterApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.mytranslator.di.application
 import com.example.mytranslator.di.historyScreen
 import com.example.mytranslator.di.mainScreen
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class InterpreterApp : Application() {
@@ -11,6 +12,7 @@ class InterpreterApp : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidContext(applicationContext)
             modules(listOf(application, mainScreen, historyScreen))
         }
     }
